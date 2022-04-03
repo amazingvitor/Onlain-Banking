b39f59c4b1ba3dee898208a66ab67213a66f103c
Vitor Hugo <vitor.h8@icloud.com>
2022-04-03 20:19:41 -0300

    Initial Commit

---
+++ Onlain Banking/Onlain_BankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onlain_BankingApp.swift
+//  Onlain Banking
+//
+//  Created by Vitor Hugo on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Onlain_BankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
