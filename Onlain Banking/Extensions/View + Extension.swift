bd638142750924f562ade7665db4691f83a712cf
Vitor Hugo <vitor.h8@icloud.com>
2022-04-03 22:34:41 -0300

    Added missing View + Extension

---
+++ Onlain Banking/Extensions/View + Extension.swift
@@ -0,0 +1,27 @@
+//
+//  View + Extension.swift
+//  Onlain Banking
+//
+//  Created by Vitor Hugo on 03/04/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+
+struct RoundedCorner: Shape {
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
